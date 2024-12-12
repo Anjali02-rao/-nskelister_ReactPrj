6b79683d827906914ab5625e3c71c41d620f8056
anjali <anjali.kodehode@gmail.com>
2024-12-12 09:40:22 +0100

    first commit

---
+++ src/KidsWishlist.jsx
@@ -0,0 +1,20 @@
+import "./KidsWishlist.css";
+
+export default function KidsWishlist({
+  data: { id, name, age, wishlist, naughty },
+}) {
+  return (
+    <div className="card">
+      <h2>{id}</h2>
+      <h2>{name}</h2>
+      <h3>({age} years)</h3>
+
+      <ul>
+        {wishlist.map((e) => (
+          <li>{e}</li>
+        ))}
+      </ul>
+      {naughty && <p>Naughty!</p>}
+    </div>
+  );
+}
